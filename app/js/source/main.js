d999b776724d17cfab41d8e4c31005f618023492
Adam Keesecker <adamkeesecker@gmail.com>
2014-04-17 12:23:54 -0500

    added chylds code alonside mine

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -1,50 +1,108 @@
 (function(){
   'use strict';
 
-  $(document).ready(initialize);
+  $(document).ready(init);
 
-  var clock = 60;
+  var clock;
   var timer;
+  var numbers;
 
-  function initialize(){
+  function init(){
     $('#start').click(start);
-    $('#shuffle').click(populate);
+    $('#board td').click(reveal);
+  }
+
+  function reveal(){
+    var row = $(this).parent().index();
+    var col = $(this).index();
+    var pos = (row * 4) + col;
+    var img = numbers[pos];
+
+    $(this).find('.back').css('background-image', 'url("./media/' + img + '.png")');
+    $(this).find('.flipper').addClass('rotate');
+    $(this).addClass('show');
+    checkMatch();
+  }
+
+  function checkMatch(){
+    var $matches = $('.show');
+
+    if($matches.length === 2){
+      var td1 = $matches[0];
+      var td2 = $matches[1];
+      var img1 = $(td1).find('.back').css('background-image');
+      var img2 = $(td2).find('.back').css('background-image');
+
+      if(img1 === img2){
+        $matches.addClass('match');
+        $matches.off('click');
+      }else{
+        setTimeout(function(){
+          $matches.find('.flipper').removeClass('rotate');
+          setTimeout(function(){
+            $matches.find('.back').css('background-image', '');
+          }, 1000);
+        }, 1000);
+      }
+
+      $matches.removeClass('show');
+    }
   }
 
   function start(){
-    // timer = setInterval(updateClock, 100);
-    var $td = $('#game > tbody > tr > td');
-    var count = $td.length;
+    create();
+    randomize();
 
-    for(var i=0; i<count; i++){
-      var $img=$('<img>');
-      var num = Math.floor(Math.random()*20)+1;
-      var td=$td[i];
-      $(td).append($img);
-      $img.attr('src', '../media/cat'+num+'.png');
+    clearInterval(timer);
+    clock = $('#clock').data('time') * 1;
+    timer = setInterval(updateClock, 1000);
+  }
+
+  function create(){
+    numbers = [];
+
+    for(var i = 0; i < 2; i++){
+      for(var j = 0; j < 10; j++){
+        numbers.push(j);
+      }
+    }
+  }
+
+  function randomize(){
+    for(var i = 0; i < numbers.length; i++){
+      var rnd = Math.floor(Math.random() * numbers.length);
+      var temp = numbers[rnd];
+      numbers[rnd] = numbers[i];
+      numbers[i] = temp;
     }
   }
 
   function updateClock(){
     clock--;
-    $('#timeDisplay').text(clock);
-    if(clock === 0){
+
+    if(clock > 0 && clock < 10){
+      warning();
+    }else if(!clock){
       clearInterval(timer);
+      results();
     }
+
+    $('#clock').text(clock);
   }
 
-  function populate(){
-    debugger;
-    timer = setInterval(updateClock, 1000);
-    var shuffled=[];
-    var $td = $('#game > tbody > tr > td');
-    var count = $td.length;
-    while(count > 0){
-      var num = Math.floor(Math.random()*count);
-      shuffled.push($td.splice(num, 1)[0]);
-      count=$td.length;
-    }
-    start();
+  function warning(){
+    var opacity = $('body').css('opacity') * 1;
+    opacity -= 0.1;
+    $('body').css('opacity', opacity);
   }
 
-}());
+  function results(){
+    var matches = $('.match').length;
+
+    if(matches === 20){
+      alert('Winner');
+    }else{
+      alert('Try Again');
+    }
+  }
+})();
