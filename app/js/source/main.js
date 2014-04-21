63127f1c627400bc621f8f4a261e7daf485c2387
Adam Keesecker <adamkeesecker@gmail.com>
2014-04-20 21:28:17 -0500

    final rough

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -1,108 +1,66 @@
 (function(){
   'use strict';
 
-  $(document).ready(init);
+  $(document).ready(initialize);
 
-  var clock;
+  var clock = 60;
   var timer;
-  var numbers;
 
-  function init(){
+  function initialize(){
     $('#start').click(start);
-    $('#board td').click(reveal);
-  }
-
-  function reveal(){
-    var row = $(this).parent().index();
-    var col = $(this).index();
-    var pos = (row * 4) + col;
-    var img = numbers[pos];
-
-    $(this).find('.back').css('background-image', 'url("./media/' + img + '.png")');
-    $(this).find('.flipper').addClass('rotate');
-    $(this).addClass('show');
-    checkMatch();
-  }
-
-  function checkMatch(){
-    var $matches = $('.show');
-
-    if($matches.length === 2){
-      var td1 = $matches[0];
-      var td2 = $matches[1];
-      var img1 = $(td1).find('.back').css('background-image');
-      var img2 = $(td2).find('.back').css('background-image');
-
-      if(img1 === img2){
-        $matches.addClass('match');
-        $matches.off('click');
-      }else{
-        setTimeout(function(){
-          $matches.find('.flipper').removeClass('rotate');
-          setTimeout(function(){
-            $matches.find('.back').css('background-image', '');
-          }, 1000);
-        }, 1000);
-      }
-
-      $matches.removeClass('show');
-    }
+    // $('#shuffle').click(populate);
   }
 
   function start(){
-    create();
-    randomize();
-
     clearInterval(timer);
-    clock = $('#clock').data('time') * 1;
-    timer = setInterval(updateClock, 1000);
-  }
+    timer = setInterval(updateClock, 100);
+    var $td = $('#game > tbody > tr > td');
+    var count = $td.length;
 
-  function create(){
-    numbers = [];
-
-    for(var i = 0; i < 2; i++){
-      for(var j = 0; j < 10; j++){
-        numbers.push(j);
-      }
+    for(var i=0; i<count; i++){
+      var $img=$('<img>');
+      // var num = Math.floor(Math.random()*count)+1;
+      // var td=$td[i];
+      var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+      var shuffledArray = array.randomize();
+      var newNum = shuffledArray.indexOf(0);
+      array.shift();
+      $img.attr('src', '../media/cat'+newNum+'.png');
     }
   }
 
-  function randomize(){
-    for(var i = 0; i < numbers.length; i++){
-      var rnd = Math.floor(Math.random() * numbers.length);
-      var temp = numbers[rnd];
-      numbers[rnd] = numbers[i];
-      numbers[i] = temp;
-    }
-  }
+
 
   function updateClock(){
     clock--;
-
-    if(clock > 0 && clock < 10){
-      warning();
-    }else if(!clock){
+    $('#timeDisplay').text(clock);
+    if(clock === 0){
       clearInterval(timer);
-      results();
-    }
-
-    $('#clock').text(clock);
-  }
-
-  function warning(){
-    var opacity = $('body').css('opacity') * 1;
-    opacity -= 0.1;
-    $('body').css('opacity', opacity);
-  }
-
-  function results(){
-    var matches = $('.match').length;
-
-    if(matches === 20){
-      alert('Winner');
-    }else{
-      alert('Try Again');
     }
   }
-})();
+
+  // function populate(){
+  //   timer = setInterval(updateClock, 1000);
+  //   var shuffled=[];
+  //   var $td = $('#game > tbody > tr > td');
+  //   var count = $td.length;
+  //   while(count > 0){
+  //     var num = Math.floor(Math.random()*count);
+  //     shuffled.push([0]);
+  //     $td.splice(num, 1);
+  //     count=$td.length;
+  //
+  //   }
+  // }
+
+
+
+  function randomize(){
+    for(var i=0; i < $td.length; i++){
+      var number = Math.floor(Math.random()*20)+1;
+      var temp = array[number];
+      numbers[number]=number[i];
+      numbers[i]=temp;
+    }
+  }
+}());
